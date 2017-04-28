fc82dea800a2157b9a3aaab43189963da89f9471
williamkeddy <WilliamKurtisEddy@gmail.com>
2017-04-27 21:34:46 -0400

    get gradle build working with spotify bundle.

--- android/app/src/main/java/com/androidspotifyplayground/MainActivity.java
+++ android/app/src/main/java/com/androidspotifyplayground/MainActivity.java
@@ -1,8 +1,13 @@
 package com.androidspotifyplayground;
 
 import com.facebook.react.ReactActivity;
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Error;
+import com.spotify.sdk.android.player.PlayerEvent;
+import com.spotify.sdk.android.player.SpotifyPlayer;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends ReactActivity
+        implements SpotifyPlayer.NotificationCallback, ConnectionStateCallback {
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -11,5 +16,40 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "AndroidSpotifyPlayground";
+    }
+
+    @Override
+    public void onLoggedIn() {
+
+    }
+
+    @Override
+    public void onLoggedOut() {
+
+    }
+
+    @Override
+    public void onLoginFailed(Error error) {
+
+    }
+
+    @Override
+    public void onTemporaryError() {
+
+    }
+
+    @Override
+    public void onConnectionMessage(String s) {
+
+    }
+
+    @Override
+    public void onPlaybackEvent(PlayerEvent playerEvent) {
+
+    }
+
+    @Override
+    public void onPlaybackError(Error error) {
+
     }
 }
