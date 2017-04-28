eb51beb12c52ca6b87100ad254b65d168fe85aae
williamkeddy <WilliamKurtisEddy@gmail.com>
2017-04-27 21:40:04 -0400

    going through spotify tutorial

--- android/app/src/main/java/com/androidspotifyplayground/MainActivity.java
+++ android/app/src/main/java/com/androidspotifyplayground/MainActivity.java
@@ -1,13 +1,17 @@
 package com.androidspotifyplayground;
 
+import android.os.Bundle;
+import android.util.Log;
 import com.facebook.react.ReactActivity;
-import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.*;
 import com.spotify.sdk.android.player.Error;
-import com.spotify.sdk.android.player.PlayerEvent;
-import com.spotify.sdk.android.player.SpotifyPlayer;
 
 public class MainActivity extends ReactActivity
         implements SpotifyPlayer.NotificationCallback, ConnectionStateCallback {
+
+    private static final String CLIENT_ID = "9ebedff68b5c47ad9368f140fd515302";
+    private static final String REDIRECT_URI = "react-native-module://callback";
+    private Player mPlayer;
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -19,37 +23,52 @@ public class MainActivity extends ReactActivity
     }
 
     @Override
-    public void onLoggedIn() {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
+    @Override
+    public void onLoggedIn() {
+        Log.d("MainActivity", "User logged in");
     }
 
     @Override
     public void onLoggedOut() {
-
+        Log.d("MainActivity", "User logged out");
     }
 
     @Override
     public void onLoginFailed(Error error) {
-
+        Log.d("MainActivity", "Login failed");
     }
 
     @Override
     public void onTemporaryError() {
-
+        Log.d("MainActivity", "Temporary error occurred");
     }
 
     @Override
-    public void onConnectionMessage(String s) {
-
+    public void onConnectionMessage(String message) {
+        Log.d("MainActivity", "Received connection message: " + message);
     }
 
     @Override
     public void onPlaybackEvent(PlayerEvent playerEvent) {
-
+        Log.d("MainActivity", "Playback event received: " + playerEvent.name());
+        switch (playerEvent) {
+            // Handle event type as necessary
+            default:
+                break;
+        }
     }
 
     @Override
     public void onPlaybackError(Error error) {
-
+        Log.d("MainActivity", "Playback error received: " + error.name());
+        switch (error) {
+            // Handle error type as necessary
+            default:
+                break;
+        }
     }
 }
